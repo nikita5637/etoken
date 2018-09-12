9f161a1b5fcfd9a0b0ce3dc23da904f5f8795416
nikita <nikita@debian>
2018-09-12 22:17:13 +0300

    Added new lib libeToken.so.9.1.7 Added new libs from opensc_0.18

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cp libeToken.so.8 libhal.so.1.0.0 /usr/lib
-ln -s /usr/lib/libeToken.so.8 /usr/lib/libeToken.so
+cp libeToken.so.8 libeToken.so.9.1.7 libhal.so.1.0.0 /usr/lib
+#ln -s /usr/lib/libeToken.so.8 /usr/lib/libeToken.so
+ln -s /usr/lib/libeToken.so.9.1.7 /usr/lib/libeToken.so
+ln -s /usr/lib/libeToken.so.9.1.7 /usr/lib/libeTPkcs11.so
 ln -s /usr/lib/libhal.so.1.0.0 /usr/lib/libhal.so.1
 
 echo "Done!"
---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm /usr/lib/libeToken.so.8 /usr/lib/libetoken.so.9.1.7 /usr/lib/libhal.so.1.0.0
+rm /usr/lib/libeToken.so
+rm /usr/lib/libeToken.so
+rm /usr/lib/libeTPkcs11.so
+rm /usr/lib/libhal.so.1
+
+echo "Done!"
